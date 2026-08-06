9108642585742367307478f3d08bd95535250d55
vishal patel <vishal.patel@kadellabs.com>
2026-08-07 01:07:02 +0530

    Fix Google Maps iframe embed URL parameter in Contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -86,10 +86,10 @@ export default function Contact() {
             </div>
           </div>
 
-          {/* Embedded Google Map Placeholder */}
+          {/* Embedded Google Map */}
           <div className="h-72 rounded-2xl overflow-hidden shadow-md border border-slate-100 dark:border-slate-800 bg-slate-200 dark:bg-slate-800">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830869428!2d-74.11976373099718!3d40.69766374874431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1652882894567!5m2!1sen!2sus"
+              src="https://maps.google.com/maps?q=Carrara,+Massa-Carrara,+Tuscany,+Italy&t=&z=12&ie=UTF8&iwloc=&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0 }}
